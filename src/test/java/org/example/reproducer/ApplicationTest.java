b2764ea6819d1f446c9d141c50a52c75e94199df
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2020-10-20 12:35:48 +0200

    Use TestContainers annotation

--- src/test/java/org/example/reproducer/ApplicationTest.java
+++ src/test/java/org/example/reproducer/ApplicationTest.java
@@ -2,11 +2,25 @@ package org.example.reproducer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@ContextConfiguration(initializers = RabbitMQContainerApplicationInitializer.class)
+@Testcontainers
 public class ApplicationTest {
+
+  @Container
+  private static RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:management");
+
+
+  @DynamicPropertySource
+  static void rabbitMQProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.rabbitmq.host", rabbitMQContainer::getContainerIpAddress);
+    registry.add("spring.rabbitmq.port", rabbitMQContainer::getAmqpPort);
+  }
 
   @Test
   public void contextLoads() {
